0d3a3821d0533a5411b3842ff986c16ce7cd1fa9
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-07-07 11:46:39 +0300

    Update timeConversion.js

--- Algorithms/timeConversion.js
+++ Algorithms/timeConversion.js
@@ -1,7 +1,4 @@
 function timeConversion(s) {
-    /*
-     * Write your code here.
-     */
     const arr = s.slice(0, 8).split(':');
     arr[0] = (s.indexOf('PM') > -1) ?
         (arr[0] == 12 ? '12' : Number(arr[0]) + 12) :
