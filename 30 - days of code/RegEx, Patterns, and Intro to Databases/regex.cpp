11a3bc6d43b85757eb63a3964ed6c933bc5e1dbe
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-07-22 16:37:04 +0300

    Create regex.cpp

---
+++ 30 - days of code/RegEx, Patterns, and Intro to Databases/regex.cpp
@@ -0,0 +1,29 @@
+//Day 28: RegEx, Patterns, and Intro to Databases
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    vector<string> names;
+    
+    string name, email;
+    regex r(".+@gmail\\.com$");
+    for (int i=0; i<N; i++)
+    {
+        cin >> name >> email;
+        if (regex_match(email,r))
+            names.push_back(name);
+    }
+    
+    sort(names.begin(), names.end());
+    int s = names.size();
+    
+    for (int i=0; i<s; i++)
+        cout << names[i]<<"\n";
+    
+    return 0;
+}
