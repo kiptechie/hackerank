e8e4c31c3a83af9a533c2af02035339dd9a12779
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-07-31 11:43:55 +0300

    Create appleAndOrange.js

---
+++ Algorithms/appleAndOrange.js
@@ -0,0 +1,56 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countApplesAndOranges function below.
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    console.log((apples || []).reduce((target, apple) => { return target + (s - a <= apple && apple <= t - a); }, 0));
+
+    console.log((oranges || []).reduce((target, orange) => { return target + (s - b <= orange && orange <= t - b); }, 0));
+}
+
+function main() {
+    const st = readLine().split(' ');
+
+    const s = parseInt(st[0], 10);
+
+    const t = parseInt(st[1], 10);
+
+    const ab = readLine().split(' ');
+
+    const a = parseInt(ab[0], 10);
+
+    const b = parseInt(ab[1], 10);
+
+    const mn = readLine().split(' ');
+
+    const m = parseInt(mn[0], 10);
+
+    const n = parseInt(mn[1], 10);
+
+    const apples = readLine().split(' ').map(applesTemp => parseInt(applesTemp, 10));
+
+    const oranges = readLine().split(' ').map(orangesTemp => parseInt(orangesTemp, 10));
+
+    countApplesAndOranges(s, t, a, b, apples, oranges);
+}
