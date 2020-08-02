47debbf11a319a58b22820db9a30762f3b36ea0e
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-08-02 19:41:20 +0300

    Create birthdayChocolate.js

---
+++ Algorithms/birthdayChocolate.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the birthday function below.
+function birthday(s, d, m) {
+    const count = (start, end, values, value) =>
+        (start < end) ? count(start + 1, end, values, value + values[start]) : value;
+
+    return s.reduce((target, item, index) =>
+        ((count(index, m + index, s || [], 0) === d) ? target + 1 : target), 0);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const dm = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d = parseInt(dm[0], 10);
+
+    const m = parseInt(dm[1], 10);
+
+    const result = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
