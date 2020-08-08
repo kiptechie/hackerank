4fc45942bf5d3f957fc4ef23d139d4f5db4c02de
Timothy Serem <theecodepoet@gmail.com>
2020-08-08 22:13:30 +0300

    Sock Merchant

---
+++ Algorithms/sockMerchant.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+
+ let pairs = 0;
+    let socks = [];
+
+    ar.forEach(item => {
+        socks[item] = !socks[item] ? 1 : socks[item] + 1;
+    });
+
+    Object.keys(socks).forEach(key => {
+        pairs += !!(socks[key] % 2) ? socks[key] - 1 : socks[key];
+    });
+
+    return pairs / 2;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
