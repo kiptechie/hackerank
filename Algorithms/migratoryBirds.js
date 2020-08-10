1b104ac500941e402a5191711e72dc9260692143
Timothy Serem <theecodepoet@gmail.com>
2020-08-10 16:59:35 +0300

    Migratory Birds

---
+++ Algorithms/migratoryBirds.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the migratoryBirds function below.
+function migratoryBirds(arr) {
+let results = new Array(5).fill(0).map((value, index) => ({ value, index: index + 1 }));
+
+    (arr || []).reduce((target, value, index) => {
+        results[value - 1].value++;
+
+        return target;
+    }, []);
+
+    return results.sort((a,b) => b.value - a.value)[0].index;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
